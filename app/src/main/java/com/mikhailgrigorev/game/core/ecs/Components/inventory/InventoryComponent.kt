6d8ac0a2ddd5c7e6dc078005162bcf4640c08d52
chu13kir <chu13kir@yandex.ru>
2020-12-30 14:47:53 +0300

    opt

--- app/src/main/java/com/mikhailgrigorev/game/core/ecs/Components/inventory/InventoryComponent.kt
+++ app/src/main/java/com/mikhailgrigorev/game/core/ecs/Components/inventory/InventoryComponent.kt
@@ -4,7 +4,8 @@ import com.mikhailgrigorev.game.core.ecs.Component
 import com.mikhailgrigorev.game.core.ecs.Components.inventory.item.Item
 
 class InventoryComponent : Component() {
-    private var items = HashMap<Int, Item>()
+    var items = HashMap<Int, Item>()
+        private set
 
     fun addItem(item: Item) {
         val inventoryItem = items[item.id]
@@ -18,7 +19,18 @@ class InventoryComponent : Component() {
 
 
     fun takeItem (item: Item) : Item? {
-        return items[item.id]
+        val invItem = items[item.id]
+        if (invItem != null && item.count <= invItem.count) {
+            invItem.take(item.count)
+            val retItem = Item(
+                invItem.id,
+                invItem.name,
+                item.count
+            )
+            if (item.count == invItem.count) items.remove(item.id)
+            return retItem
+        }
+        return null
     }
 
     fun getAllItems() : MutableCollection<Item> {
